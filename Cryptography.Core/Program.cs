af22ddbd7abb78929edfffcd1eec7dfe4e5243b4
Julian Hüpppauff <julian@hueppauff.info>
2018-06-28 09:52:43 +0200

    fixed typo

---
+++ Cryptography.Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cryptography.Core
+{
+    class Program
+    {
+        private const string key = "E546C8DF278CD5931069B522E695D4F2";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert String to encrypt");
+            string content = Console.ReadLine();
+
+            Console.WriteLine("--------------");
+            var encrypted = Cryptography.EncryptString(content, key);
+            Console.WriteLine($"encrypted:  {encrypted}");
+
+            var decrypted = Cryptography.DecryptString(encrypted, key);
+            Console.WriteLine($"decrypted: {decrypted}");
+
+            Console.ReadLine();
+        }
+    }
+}
